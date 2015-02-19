bdfbcc89b1a6c95c44699f6865442e9348969e64
Mopk <mopk2002@yandex.ru>
2015-02-19 20:25:43 +0300

    Made 1st Spring-IoC (DI) app.

---
+++ src/org/mopk/Main.java
@@ -0,0 +1,12 @@
+package org.mopk;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("/myFirstSpringAppCtx.xml");
+    }
+}
---
+++ src/org/mopk/SuperClassService.java
@@ -0,0 +1,21 @@
+package org.mopk;
+
+/**
+ * Created by: alit0714
+ * Date: 2015-02-18
+ * Time: 16:45
+ */
+public class SuperClassService {
+
+    static {
+        System.out.println("A test of a static block.");
+    }
+
+    public SuperClassService() {
+        System.out.println("A test of a constructor.");
+    }
+
+    public void doSomething() {
+
+    }
+}
